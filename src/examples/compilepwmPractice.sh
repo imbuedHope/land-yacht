292494cafc3d49a5c2ba73b6856f543765991643
Sanjit Dutta <Sanjit Dutta>
2016-04-14 16:59:04 -0500

    updated readme, starting PWM sample code

---
+++ src/examples/compilepwmPractice.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+g++ pwmPractice.cpp ../exploringBBlibrary/libEBBLibrary.so -o pwmPractice -I ../exploringBBlibrary/
---
+++ src/examples/pwmPractice.cpp
@@ -0,0 +1,15 @@
+/* A Simple GPIO application
+* Written by Derek Molloy for the book "Exploring BeagleBone: Tools and 
+* Techniques for Building with Embedded Linux" by John Wiley & Sons, 2014
+* ISBN 9781118935125. Please see the file README.md in the repository root 
+* directory for copyright and GNU GPLv3 license information.            */
+
+#include <iostream>
+#include <unistd.h> //for usleep
+#include "pwm/PWM.h"
+using namespace exploringBB;
+using namespace std;
+
+int main(){
+   return 0;
+}
