1fd87fb649a5f3ad7e45687cf5352a75449c5148
Sriram Raghu <imbuedhope@gmail.com>
2016-05-10 11:49:25 -0500

    added volatile keyword to thread vars

--- src/server/comn.h
+++ src/server/comn.h
@@ -7,7 +7,7 @@
 #define HALT_STATE 1
 #define RUN_STATE 0
 
-extern char halt_system;
-extern int comn_dat[COMN_DAT_LEN];
+extern volatile char halt_system;
+extern volatile int comn_dat[COMN_DAT_LEN];
 
 extern pthread_mutex_t comn_mutex;
